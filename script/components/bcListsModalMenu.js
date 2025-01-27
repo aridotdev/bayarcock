46c05e8f83a88a0b87b8d0457616e4675693e09c
arisafari.ridwan <arisafari.ridwan@gmail.com>
2025-01-27 16:29:20 +0700

    fix: add save fitur on modal menu

--- script/components/bcListsModalMenu.js
+++ script/components/bcListsModalMenu.js
@@ -30,8 +30,8 @@ export default {
             </label>
             <button 
               class="flex-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              @click.prevent="save()"
-              >Simpan</button>
+              @click.prevent="done()"
+              >Done</button>
           </li>
           <li>
             <button
@@ -61,7 +61,7 @@ export default {
       this.$emit('emitRemovePlayer')
     },
 
-    save(){
+    done(){
       this.player.menuIsHidden = true
     }
   }
